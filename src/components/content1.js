7a07e725cad0fcfea91968225abd1a7415e4c640
Cyril JACQUIOD <cyriljacquiod@macbook-air-de-cyril.home>
2023-01-30 18:23:20 +0100

    second

---
+++ src/components/content1.js
@@ -0,0 +1,18 @@
+const content1 = (props) => {
+  console.log(props);
+  return (
+    <p classDescription={props}>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
+      porttitor aliquet risus, sed venenatis dui rutrum sed. Praesent ultrices
+      feugiat felis ac volutpat. Etiam id rhoncus dui. Praesent porttitor massa
+      vel massa efficitur, ac aliquet magna hendrerit. Cras convallis sodales
+      eros a molestie. Nullam ipsum tellus, egestas quis dolor facilisis, rutrum
+      gravida purus. Morbi ut sodales ligula. Sed lacus risus, sagittis ac
+      lobortis eget, semper ornare orci. Aenean vehicula lobortis nisi a
+      laoreet. Aliquam quis dictum augue. Cras sit amet lacinia sem.
+      Pellentesque porta elit at purus sollicitudin, at tempor tortor vulputate.
+    </p>
+  );
+};
+
+export default content1;
